2290a120b65ae07c0ce2e6d7dc39c67168557199
Ariel Alvarez <arialvarez2019@gmail.com>
2023-03-20 23:33:20 -0300

    primer commit

---
+++ algoritmo.js
@@ -0,0 +1,10 @@
+document.querySelector("body").innerHTML = `
+<h1>Titulo de la practica 1-1</h1>
+    <h2>Titulo h2</h2>
+    <h3>Titulo h3</h3>
+
+    <img src="https://th.bing.com/th/id/R.5bd1d749aaf60d4e8d222ce31a1a6153?rik=WfvWjV582EMoKg&riu=http%3a%2f%2fhdfreewallpaper.net%2fwp-content%2fuploads%2f2017%2f10%2fdarth-vader.png&ehk=Zo9gT1DQCdtP%2b%2beK61Vc2ijd6HEWv7O5HI1BEVlMspc%3d&risl=&pid=ImgRaw&r=0"
+        alt="dark vader 2">
+
+        <img src="imagenes/dark V.png" alt="dark vader 1"
+`;
